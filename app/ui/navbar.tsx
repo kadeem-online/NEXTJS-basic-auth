d74a434f5ba9fdc2079f8769395ee8d96da4e3b0
kadeem254 <kadeemalexander254@gmail.com>
2024-09-05 01:33:20 +0300

    UPDATE: added the signup route to the navbar.

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -4,10 +4,23 @@ export default function Navbar() {
 	return (
 		<div className="py-5 border-b border-gray-900 bg-gray-950 sticky">
 			<div className="container">
-				<nav className="font-medium text- text-orange-300">
-					<ul className="nav-list flex justify-center items-center">
+				<nav className="font-semibold text-orange-300">
+					<ul className="nav-list flex justify-center items-center gap-4">
 						<li>
-							<Link href="/">Home</Link>
+							<Link
+								href="/"
+								className="underline underline-offset-4 hover:text-purple-300"
+							>
+								Home
+							</Link>
+						</li>
+						<li>
+							<Link
+								href="/signup"
+								className="underline underline-offset-4 hover:text-purple-300"
+							>
+								Sign Up
+							</Link>
 						</li>
 					</ul>
 				</nav>
