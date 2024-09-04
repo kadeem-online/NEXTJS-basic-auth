ed47ac277827766281161f84687900e7c065f147
kadeem254 <kadeemalexander254@gmail.com>
2024-09-04 17:07:47 +0300

    UPDATE; created first model and migration for sqlite database

---
+++ lib/models/user.ts
@@ -0,0 +1,62 @@
+import {
+	CreationOptional,
+	DataTypes,
+	InferAttributes,
+	InferCreationAttributes,
+	Model,
+	Sequelize,
+} from "sequelize";
+
+class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
+	declare id: number;
+	declare name: string;
+	declare email: string;
+	declare password: string;
+	declare createdAt: CreationOptional<Date>;
+	declare updatedAt: CreationOptional<Date>;
+}
+
+/**
+ * Initialize the User model for use alongside a sequelize instance.
+ *
+ * @param sequelize_instance {Sequelize} - The instance of sequelize to run this
+ * create this model on.
+ */
+function initializeUserModel(sequelize_instance: Sequelize) {
+	User.init(
+		{
+			id: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+				allowNull: false,
+			},
+			name: {
+				type: DataTypes.STRING(255),
+				allowNull: false,
+			},
+			email: {
+				type: DataTypes.STRING(255),
+				allowNull: false,
+				unique: true,
+			},
+			password: {
+				type: DataTypes.STRING(255),
+				allowNull: false,
+			},
+			createdAt: {
+				type: DataTypes.DATE,
+			},
+			updatedAt: {
+				type: DataTypes.DATE,
+			},
+		},
+		{
+			sequelize: sequelize_instance,
+			tableName: "users",
+			timestamps: true,
+		}
+	);
+}
+
+export { User as default };
---
+++ migrations/20240904140038-init.js
@@ -0,0 +1,43 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable("users", {
+			id: {
+				type: Sequelize.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+				allowNull: false,
+			},
+			name: {
+				type: Sequelize.STRING(255),
+				allowNull: false,
+			},
+			email: {
+				type: Sequelize.STRING(255),
+				allowNull: false,
+				unique: true,
+			},
+			password: {
+				type: Sequelize.STRING(255),
+				allowNull: false,
+			},
+			createdAt: {
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				type: Sequelize.DATE,
+			},
+		});
+	},
+
+	async down(queryInterface, Sequelize) {
+		/**
+		 * Add reverting commands here.
+		 *
+		 * Example:
+		 * await queryInterface.dropTable('users');
+		 */
+	},
+};
