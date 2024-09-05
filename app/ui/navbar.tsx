5aba158e374189382464326cc152faadd64b34c0
kadeem254 <kadeemalexander254@gmail.com>
2024-09-05 23:03:43 +0300

    UPDATE: updated the navbar to show routes based on session

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -1,27 +1,47 @@
+import { verifySession } from "@/lib/sessions";
 import Link from "next/link";
+import LogoutForm from "./logout-form";
 
-export default function Navbar() {
+function SignOutButton() {}
+
+export default async function Navbar() {
+	const session = await verifySession();
+
+	const nav_links_list = [
+		{ href: "/", label: "Home", visible: true },
+		{ href: "/profile", label: "Profile", visible: session !== null },
+		{ href: "/login", label: "Sign In", visible: session === null },
+		{ href: "/signup", label: "Sign Up", visible: session === null },
+	];
+
 	return (
 		<div className="py-5 border-b border-gray-900 bg-gray-950 sticky">
 			<div className="container">
 				<nav className="font-semibold text-orange-300">
 					<ul className="nav-list flex justify-center items-center gap-4">
-						<li>
-							<Link
-								href="/"
-								className="underline underline-offset-4 hover:text-purple-300"
-							>
-								Home
-							</Link>
-						</li>
-						<li>
-							<Link
-								href="/signup"
-								className="underline underline-offset-4 hover:text-purple-300"
-							>
-								Sign Up
-							</Link>
-						</li>
+						{nav_links_list.map((navlink, index) => {
+							if (navlink.visible) {
+								return (
+									<li key={index}>
+										<Link
+											href={navlink.href}
+											className="underline underline-offset-4 hover:text-purple-300"
+										>
+											{navlink.label}
+										</Link>
+									</li>
+								);
+							}
+						})}
+
+						{session !== null && (
+							<li key={"logout"}>
+								<LogoutForm
+									label="Sign Out"
+									className="underline underline-offset-4 hover:text-purple-300"
+								/>
+							</li>
+						)}
 					</ul>
 				</nav>
 			</div>
