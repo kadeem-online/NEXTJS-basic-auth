bb10b02653d3d87f045715af2023317d0e74a1b0
kadeem254 <kadeemalexander254@gmail.com>
2024-09-09 11:17:47 +0300

    UPDATE: added context for react hot toast to the top level of the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./ui/navbar";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
 		<html lang="en">
 			<body className={`${inter.className} min-h-screen relative`}>
 				<Navbar />
+				<Toaster position="bottom-left" />
 				{children}
 			</body>
 		</html>
